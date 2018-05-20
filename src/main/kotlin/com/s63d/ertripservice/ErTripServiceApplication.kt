951f65b887393035263504eeaaf12f36dda1d3f5
Marvin <marvin.zwolsman@gmail.com>
2018-05-20 22:52:07 +0200

    Corrected name

--- src/main/kotlin/com/s63d/ertripservice/ErTripServiceApplication.kt
+++ src/main/kotlin/com/s63d/ertripservice/ErTripServiceApplication.kt
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableFeignClients
-class ErVehicleServiceApplication
+class ErTripServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<ErVehicleServiceApplication>(*args)
+    runApplication<ErTripServiceApplication>(*args)
 }
