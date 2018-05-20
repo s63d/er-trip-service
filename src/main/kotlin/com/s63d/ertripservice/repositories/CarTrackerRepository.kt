36fa691794fa131b4f25c1c3ebcdff58b2a509b2
Marvin <marvin.zwolsman@gmail.com>
2018-05-20 23:07:07 +0200

    Removed cartracker service & repository Not needed since it's embedded in Trip

--- src/main/kotlin/com/s63d/ertripservice/repositories/CarTrackerRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.s63d.ertripservice.repositories
-
-import com.s63d.ertripservice.domain.CarTracker
-import org.springframework.data.repository.CrudRepository
-
-interface CarTrackerRepository : CrudRepository<CarTracker, String>
--- src/main/kotlin/com/s63d/ertripservice/services/CarTrackerService.kt
+++
@@ -1,9 +0,0 @@
-package com.s63d.ertripservice.services
-
-import com.s63d.ertripservice.repositories.CarTrackerRepository
-import org.springframework.stereotype.Service
-
-@Service
-class CarTrackerService(private val carTrackerRepository: CarTrackerRepository) {
-
-}
