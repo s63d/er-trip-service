69ed52bd1c0087ba7097f3be0fdcf926ebe49a98
Marvin <marvin.zwolsman@gmail.com>
2018-05-20 23:08:38 +0200

    Removed CarTrackerService injection

--- src/main/kotlin/com/s63d/ertripservice/services/TripService.kt
+++ src/main/kotlin/com/s63d/ertripservice/services/TripService.kt
@@ -6,7 +6,7 @@ import com.s63d.ertripservice.repositories.TripRepository
 import org.springframework.stereotype.Service
 
 @Service
-class TripService(private val tripRepository: TripRepository, private val carTrackerService: CarTrackerService) {
+class TripService(private val tripRepository: TripRepository) {
 
     fun createNew(carTrackerId: String) : Trip {
         val trip = Trip(CarTracker(carTrackerId))
