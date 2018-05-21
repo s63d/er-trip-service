156a3be8b8b5e60440981e791eb367761e6d595e
Marvin <marvin.zwolsman@gmail.com>
2018-05-21 11:39:17 +0200

    Add the rest of the properties

--- src/main/kotlin/com/s63d/ertripservice/domain/TripResponse.kt
+++ src/main/kotlin/com/s63d/ertripservice/domain/TripResponse.kt
@@ -1,5 +1,7 @@
 package com.s63d.ertripservice.domain
 
-data class TripResponse (val tripId: Long, val length: Int, val polyline: String) {
-    constructor(trip: Trip) : this(trip.id, trip.locations.totalLength, trip.locations.toPolyline())
+import java.util.*
+
+data class TripResponse (val tripId: Long, val length: Int, val polyline: String, val date: Date, val carTrackerId: String) {
+    constructor(trip: Trip) : this(trip.id, trip.locations.totalLength, trip.locations.toPolyline(), trip.date, trip.carTracker.carTrackerId)
 }
