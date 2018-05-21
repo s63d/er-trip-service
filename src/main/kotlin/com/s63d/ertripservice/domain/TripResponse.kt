fb30fb65c03835fbac1ba49665a0d193707880f0
Marvin <marvin.zwolsman@gmail.com>
2018-05-21 11:37:48 +0200

    cleanup

--- src/main/kotlin/com/s63d/ertripservice/domain/TripResponse.kt
+++ src/main/kotlin/com/s63d/ertripservice/domain/TripResponse.kt
@@ -1,13 +1,5 @@
 package com.s63d.ertripservice.domain
 
-import kotlin.math.roundToInt
-
 data class TripResponse (val tripId: Long, val length: Int, val polyline: String) {
-    companion object {
-        private fun computePolyline(locations: Iterable<Location>) : String {
-            return ""
-        }
-
-    }
     constructor(trip: Trip) : this(trip.id, trip.locations.totalLength, trip.locations.toPolyline())
 }
