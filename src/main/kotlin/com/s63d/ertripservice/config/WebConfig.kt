d3ff44464cac841a0020bac1109500371131caa0
Marvin <marvin.zwolsman@gmail.com>
2018-05-21 14:00:38 +0200

    Corrected package so it has CORS support

--- src/main/kotlin/com/s63d/ertripservice/config/WebConfig.kt
+++ src/main/kotlin/com/s63d/ertripservice/config/WebConfig.kt
@@ -1,4 +1,4 @@
-package com.s63d.ervehicleservice.config
+package com.s63d.ertripservice.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
