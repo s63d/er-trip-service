555541b7521191d5e8ce54f6bff20898a0d1a28c
Marvin <marvin.zwolsman@gmail.com>
2018-06-06 13:42:17 +0200

    Cleanup

--- src/main/kotlin/com/s63d/ertripservice/controllers/TripController.kt
+++ src/main/kotlin/com/s63d/ertripservice/controllers/TripController.kt
@@ -10,19 +10,8 @@ class TripController(private val tripService: TripService) {
 
     @PostMapping
     fun createNew(@RequestParam carTrackerId: String) = tripService.createNew(carTrackerId)
-//
-//    @GetMapping
-//    fun getByCarTracker(@RequestParam trackerId: String) = tripService.byCarTracker(trackerId)
-//
-//    @GetMapping("{id}")
-//    fun getByLicense(@PathVariable id: String) =
-//            if(id.toLongOrNull() != null)
-//                tripService.byId(id.toLong())
-//            else
-//                tripService.byVehicleId(id)
 
     @GetMapping
-    fun getTripsForVehicle(@RequestParam vehicleId: String, principal: Principal) : Any {
-        return tripService.byVehicleId(vehicleId)
-    }
+    fun getTripsForVehicle(@RequestParam vehicleId: String, principal: Principal) = tripService.byVehicleId(vehicleId)
+
 }
